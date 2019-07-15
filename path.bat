a990dbcbe02b13c828ba952658a824ec84a1d0af
bimManager <kirillkozlov@ymail.com>
2019-07-15 09:14:26 +0300

    added path.bat

---
+++ path.bat
@@ -0,0 +1,18 @@
+set Path=^
+%WINDIR%\system32;^
+%WINDIR%;^
+%WINDIR%\System32\Wbem;^
+%WINDIR%\System32\WindowsPowerShell\v1.0\;^
+%WINDIR%\System32\OpenSSH\;^
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;^
+%USERPROFILE%\AppData\Local\Programs\Microsoft VS Code\bin;^
+%PROGRAMFILES%\Autodesk\Revit 2019;^
+X:\kkozlov\7-Zip;^
+%PROGRAMFILES(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\x64;^
+%PROGRAMFILES%\Microsoft Office\root\Office16;^
+%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd;^
+%PROGRAMFILES(x86)%\Windows Kits\10\Debuggers\x64;^
+%PROGRAMFILES%\Autodesk\Navisworks Manage 2019;^
+%PROGRAMFILES(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools;^
+%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\NodeJs\win-x64;^
+%PROGRAMFILES(x86)%\Google\Chrome\Application
